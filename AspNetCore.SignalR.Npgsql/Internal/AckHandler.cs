d2b9face1657352d5549861f4eabbf244ec02cc1
Cherie Pun <cherie.pun@optimizely.com>
2025-07-11 09:38:06 +0100

    Make Ackhandler more similar

--- AspNetCore.SignalR.Npgsql/Internal/AckHandler.cs
+++ AspNetCore.SignalR.Npgsql/Internal/AckHandler.cs
@@ -2,89 +2,89 @@ using System.Collections.Concurrent;
 namespace AspNetCore.SignalR.Npgsql.Internal;
 
 internal class AckHandler : IDisposable
+{
+    private readonly ConcurrentDictionary<int, AckInfo> _acks = new();
+    private readonly Timer _timer;
+    private readonly TimeSpan _ackThreshold = TimeSpan.FromSeconds(30);
+    private readonly TimeSpan _ackInterval = TimeSpan.FromSeconds(5);
+    private readonly object _lock = new object();
+    private bool _disposed;
+
+    public AckHandler()
     {
-        private readonly ConcurrentDictionary<int, AckInfo> _acks = new();
-        private readonly Timer _timer;
-        private readonly TimeSpan _ackThreshold = TimeSpan.FromSeconds(30);
-        private readonly TimeSpan _ackInterval = TimeSpan.FromSeconds(5);
-        private readonly object _lock = new object();
-        private bool _disposed;
+        _timer = NonCapturingTimer.Create(state => ((AckHandler)state!).CheckAcks(), state: this, dueTime: _ackInterval, period: _ackInterval);
+    }
 
-        public AckHandler()
-        {
-            _timer = NonCapturingTimer.Create(state => ((AckHandler)state!).CheckAcks(), state: this, dueTime: _ackInterval, period: _ackInterval);
-        }
-
-        public Task CreateAck(int id)
-        {
-            lock (_lock)
-            {
-                if (_disposed)
-                {
-                    return Task.CompletedTask;
-                }
-
-                return _acks.GetOrAdd(id, _ => new AckInfo()).Tcs.Task;
-            }
-        }
-
-        public void TriggerAck(int id)
-        {
-            if (_acks.TryRemove(id, out var ack))
-            {
-                ack.Tcs.TrySetResult(true);
-            }
-        }
-
-        private void CheckAcks()
+    public Task CreateAck(int id)
+    {
+        lock (_lock)
         {
             if (_disposed)
             {
-                return;
+                return Task.CompletedTask;
             }
 
-            var utcNow = DateTime.UtcNow;
+            return _acks.GetOrAdd(id, _ => new AckInfo()).Tcs.Task;
+        }
+    }
 
-            foreach (var pair in _acks)
-            {
-                var elapsed = utcNow - pair.Value.Created;
-                if (elapsed > _ackThreshold)
-                {
-                    if (_acks.TryRemove(pair.Key, out var ack))
-                    {
-                        ack.Tcs.TrySetCanceled();
-                    }
-                }
-            }
+    public void TriggerAck(int id)
+    {
+        if (_acks.TryRemove(id, out var ack))
+        {
+            ack.Tcs.TrySetResult(true);
+        }
+    }
+
+    private void CheckAcks()
+    {
+        if (_disposed)
+        {
+            return;
         }
 
-        public void Dispose()
+        var utcNow = DateTime.UtcNow;
+
+        foreach (var pair in _acks)
         {
-            lock (_lock)
+            var elapsed = utcNow - pair.Value.Created;
+            if (elapsed > _ackThreshold)
             {
-                _disposed = true;
-
-                _timer.Dispose();
-
-                foreach (var pair in _acks)
+                if (_acks.TryRemove(pair.Key, out var ack))
                 {
-                    if (_acks.TryRemove(pair.Key, out var ack))
-                    {
-                        ack.Tcs.TrySetCanceled();
-                    }
+                    ack.Tcs.TrySetCanceled();
                 }
-            }
-        }
-
-        private class AckInfo
-        {
-            public TaskCompletionSource<bool> Tcs { get; private set; }
-            public DateTime Created { get; private set; }
-
-            public AckInfo()
-            {
-                Created = DateTime.UtcNow;
-                Tcs = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
             }
         }
     }
+
+    public void Dispose()
+    {
+        lock (_lock)
+        {
+            _disposed = true;
+
+            _timer.Dispose();
+
+            foreach (var pair in _acks)
+            {
+                if (_acks.TryRemove(pair.Key, out var ack))
+                {
+                    ack.Tcs.TrySetCanceled();
+                }
+            }
+        }
+    }
+
+    private class AckInfo
+    {
+        public TaskCompletionSource<bool> Tcs { get; private set; }
+        public DateTime Created { get; private set; }
+
+        public AckInfo()
+        {
+            Created = DateTime.UtcNow;
+            Tcs = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+        }
+    }
+}
--- src/AspNetCore.SignalR.Postgres/Internal/DefaultHubMessageSerializer.cs
+++ src/AspNetCore.SignalR.Postgres/Internal/DefaultHubMessageSerializer.cs
@@ -4,35 +4,33 @@
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.AspNetCore.SignalR.Protocol;
 
-namespace AspNetCore.SignalR.Postgres.Internal
+namespace AspNetCore.SignalR.Postgres.Internal;
+internal class DefaultHubMessageSerializer
 {
-    internal class DefaultHubMessageSerializer
+    private readonly List<IHubProtocol> _hubProtocols;
+
+    public DefaultHubMessageSerializer(IHubProtocolResolver hubProtocolResolver, IList<string>? globalSupportedProtocols, IList<string>? hubSupportedProtocols)
     {
-        private readonly List<IHubProtocol> _hubProtocols;
-
-        public DefaultHubMessageSerializer(IHubProtocolResolver hubProtocolResolver, IList<string>? globalSupportedProtocols, IList<string>? hubSupportedProtocols)
+        var supportedProtocols = hubSupportedProtocols ?? globalSupportedProtocols ?? Array.Empty<string>();
+        _hubProtocols = new List<IHubProtocol>(supportedProtocols.Count);
+        foreach (var protocolName in supportedProtocols)
         {
-            var supportedProtocols = hubSupportedProtocols ?? globalSupportedProtocols ?? Array.Empty<string>();
-            _hubProtocols = new List<IHubProtocol>(supportedProtocols.Count);
-            foreach (var protocolName in supportedProtocols)
+            var protocol = hubProtocolResolver.GetProtocol(protocolName, (supportedProtocols as IReadOnlyList<string>) ?? supportedProtocols.ToList());
+            if (protocol != null)
             {
-                var protocol = hubProtocolResolver.GetProtocol(protocolName, (supportedProtocols as IReadOnlyList<string>) ?? supportedProtocols.ToList());
-                if (protocol != null)
-                {
-                    _hubProtocols.Add(protocol);
-                }
+                _hubProtocols.Add(protocol);
             }
         }
+    }
 
-        public IReadOnlyList<SerializedMessage> SerializeMessage(HubMessage message)
+    public IReadOnlyList<SerializedMessage> SerializeMessage(HubMessage message)
+    {
+        var list = new List<SerializedMessage>(_hubProtocols.Count);
+        foreach (var protocol in _hubProtocols)
         {
-            var list = new List<SerializedMessage>(_hubProtocols.Count);
-            foreach (var protocol in _hubProtocols)
-            {
-                list.Add(new SerializedMessage(protocol.Name, protocol.GetMessageBytes(message)));
-            }
-
-            return list;
+            list.Add(new SerializedMessage(protocol.Name, protocol.GetMessageBytes(message)));
         }
+
+        return list;
     }
 }
